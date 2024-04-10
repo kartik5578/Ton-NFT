230f4b664dbd20d6e44c0e2df9ce38a9e0f00600
Kunal Patel <kunalpatel88843@gmail.com>
2024-04-10 15:03:56 +0530

    fix:Create and Home fixed

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,51 +1,56 @@
-import  { useEffect, useState } from 'react';
-import Cards from './Cards';
+import { useEffect, useState } from "react";
+import Cards from "./Cards";
 // import Info from './NFTinfo';
 // import { useMainContract } from '../hooks/useMainContract';
 
 interface Item {
-  ipfs_pin_hash: string
+    ipfs_pin_hash: string;
+}
+
+interface dataItem {
+    count: number;
+    rows: [];
 }
 
 function Home() {
-  // const { sendIncrement } = useMainContract();
-  const [items, setItems] = useState<Item[]>([]);
-  // const [toggle, setToggle] = useState<boolean>(false)
-  // const [nftitem, setNftitem] = useState<any>(null)
+    // const { sendIncrement } = useMainContract();
+    const [items, setItems] = useState<Item[]>([]);
+    // const [toggle, setToggle] = useState<boolean>(false)
+    // const [nftitem, setNftitem] = useState<any>(null)
 
-  useEffect(() => {
-    const uri: string = 'https://api.pinata.cloud/data/pinList';
+    useEffect(() => {
+        const uri: string = "https://api.pinata.cloud/data/pinList";
 
-    // Define headers
-    const header: HeadersInit = {
-      'Content-Type': 'application/json',
-      'pinata_api_key': `dedc16b75664bc108136`,
-      'pinata_secret_api_key': `2b8fc356900683c32c2c59356ce60d2a97d2561b2d89d95efda9d02b399e44c8`
-    };
+        // Define headers
+        const header: HeadersInit = {
+            "Content-Type": "application/json",
+            pinata_api_key: `dedc16b75664bc108136`,
+            pinata_secret_api_key: `2b8fc356900683c32c2c59356ce60d2a97d2561b2d89d95efda9d02b399e44c8`,
+        };
 
-    fetch(uri, {
-      method: 'GET',
-      headers: header
-    })
-      .then((response: Response) => {
-        if (response.ok) {
-          // Parse the JSON response
-          return response.json();
-        }
-        throw new Error('Network response was not ok.');
-      })
-      .then((files: any) => {
-        setItems(files.rows);
-        console.log(files);
-      })
-      .catch((error: Error) => {
-        console.error('Error:', error);
-      });
-  }, []);
+        fetch(uri, {
+            method: "GET",
+            headers: header,
+        })
+            .then((response: Response) => {
+                if (response.ok) {
+                    // Parse the JSON response
+                    return response.json();
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then((files: dataItem) => {
+                setItems(files.rows);
+                console.log(files);
+            })
+            .catch((error: Error) => {
+                console.error("Error:", error);
+            });
+    }, []);
 
-  return (
-    <>
-      {/* {toggle ? <Info  setToggle={setToggle} nftitem={nftitem}/> :
+    return (
+        <>
+            {/* {toggle ? <Info  setToggle={setToggle} nftitem={nftitem}/> :
        <div className='flex flex-wrap gradient-bg-welcome min-h-screen  gap-10 justify-center pt-24 pb-5 px-16'>
         {items.map((item: any)=>(
           <Cards item={item} openNFT={openNFT}/>
@@ -54,32 +59,27 @@ function Home() {
         </div>
   } */}
 
-      <div className='flex flex-wrap gradient-bg-welcome min-h-screen  gap-10 justify-center pt-24 pb-5 px-16'>
-        {items.map((item: Item, index: number) => (
-          <Cards key={index} item={item} />
-        ))}
-      </div>
-    </>
-  );
+            <div className="flex flex-wrap gradient-bg-welcome min-h-screen  gap-10 justify-center pt-24 pb-5 px-16">
+                {items.map((item: Item, index: number) => (
+                    <Cards key={index} item={item} />
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default Home;
-
 
 // import React, { useEffect, useState } from 'react';
 // import Cards from './Cards';
 // // import Info from './NFTinfo';
 // import { useMainContract } from '../hooks/useMainContract';
 
-
-
 // function Home() {
 //   const{sendIncrement} = useMainContract();
 //   const [items, setItems] = useState<Array>([]);
 //   // const [toggle, setToggle] = useState<boolean>(false)
 //   // const [nftitem, setNftitem] = useState<any>(null)
-
- 
 
 //   useEffect(()=>{
 //     const uri: string = 'https://api.pinata.cloud/data/pinList';
@@ -90,7 +90,6 @@ export default Home;
 //     'pinata_api_key': `dedc16b75664bc108136`,
 //     'pinata_secret_api_key': `2b8fc356900683c32c2c59356ce60d2a97d2561b2d89d95efda9d02b399e44c8`
 //     };
-
 
 //     fetch(uri, {
 //     method: 'GET',
@@ -111,11 +110,10 @@ export default Home;
 //         console.error('Error:', error);
 //     });
 //   },[])
-  
 
 //   // async function openNFT(data:any) {
 //   //   console.log(data)
-   
+
 //   //  const res = await sendIncrement(0.05).then(result => {
 
 //   //     setNftitem(data);
@@ -150,17 +148,15 @@ export default Home;
 //   // }).catch(err =>{
 //   //   console.log(err);
 //   // });
-       
-  
-//   // you can use signed boc to find the transaction 
+
+//   // you can use signed boc to find the transaction
 //   // const someTxData = await myAppExplorerService.getTransaction(result.boc);
 //   // alert('Transaction was sent successfully', someTxData);
 
 // // } catch (e) {
 // //   console.error(e);
-// // }    
+// // }
 //   // }
-  
 
 //   return (
 //     <>
@@ -168,7 +164,7 @@ export default Home;
 //      <div className='flex flex-wrap gradient-bg-welcome min-h-screen  gap-10 justify-center pt-24 pb-5 px-16'>
 //       {items.map((item: any)=>(
 //         <Cards item={item} openNFT={openNFT}/>
-        
+
 //       ))}
 //       </div>
 // } */}
@@ -176,7 +172,7 @@ export default Home;
 // <div className='flex flex-wrap gradient-bg-welcome min-h-screen  gap-10 justify-center pt-24 pb-5 px-16'>
 //       {items.map((item: any)=>(
 //         <Cards item={item} />
-        
+
 //       ))}
 //       </div>
 //     </>
